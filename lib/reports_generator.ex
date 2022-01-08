29420b1dea7b2245382f5a7c18d4adcf668c0376
alexandra <alexandrabsouz@gmail.com>
2022-01-08 16:34:54 -0300

    refactor: reading file on a stream

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,12 +2,18 @@ defmodule ReportsGenerator do
 
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
   end
 
-  defp handle_file({:ok, result}), do: result
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
 
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
-  
 end
+
+
+# ReportsGenerator.build("report_1.csv")
