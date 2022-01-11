76a920103dc3509c1c44d2db58450e61e34a82be
alexandra <alexandrabsouz@gmail.com>
2022-01-11 20:34:06 -0300

    feat: build report function

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -18,18 +18,28 @@ defmodule ReportsGenerator do
   ]
 
   def build(filename) do
-    filename
-    |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+    result =
+      filename
+      |> Parser.parse_file()
+      |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+
+    {:ok, result}
+  end
+
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "not is a filenames list"}
   end
 
   def build_from_many(filenames) do
-    filenames
-    |> Task.async_stream(&build/1)
-    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
   end
 
-  defp sum_values([id, food_name, price], %{users: users, foods: foods}) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
     build_report(foods, users)
@@ -43,16 +53,11 @@ defmodule ReportsGenerator do
 
   def fetch_higher_cost(_report, _option), do: {:error, "inválid option!"}
 
-  defp sum_reports(
-         %{
-           foods: foods1,
-           users: users1
-         },
-         %{
-           foods: foods2,
-           users: users2
-         }
-       ) do
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+      "foods" => foods2,
+      "users" => users2
+    }) do
+
     foods = merge_maps(foods1, foods2)
     users = merge_maps(users1, users2)
 
@@ -69,7 +74,7 @@ defmodule ReportsGenerator do
     build_report(foods, users)
   end
 
-  defp build_report(foods, users), do: %{users: users, foods: foods}
+  defp build_report(foods, users), do: %{"users" => users, "foods" => foods}
 end
 
 # ReportsGenerator.build("report_test.csv")
