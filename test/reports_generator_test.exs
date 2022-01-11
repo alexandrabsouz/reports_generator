c5f707236e80bf104e6ce9a06d27b36c6249846a
alexandra <alexandrabsouz@gmail.com>
2022-01-11 20:55:59 -0300

    test: list summing using parallelism

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -9,49 +9,9 @@ defmodule ReportsGeneratorTest do
       # EXERCISE
       response = ReportsGenerator.build(file_name)
 
-      expected_response = %{
-        "foods" => %{
-          "açaí" => 1,
-          "churrasco" => 2,
-          "esfirra" => 3,
-          "hambúrguer" => 2,
-          "pastel" => 0,
-          "pizza" => 2,
-          "prato_feito" => 0,
-          "sushi" => 0
-        },
-        "users" => %{
-          "1" => 48,
-          "10" => 36,
-          "11" => 0,
-          "12" => 0,
-          "13" => 0,
-          "14" => 0,
-          "15" => 0,
-          "16" => 0,
-          "17" => 0,
-          "18" => 0,
-          "19" => 0,
-          "2" => 45,
-          "20" => 0,
-          "21" => 0,
-          "22" => 0,
-          "23" => 0,
-          "24" => 0,
-          "25" => 0,
-          "26" => 0,
-          "27" => 0,
-          "28" => 0,
-          "29" => 0,
-          "3" => 31,
-          "30" => 0,
-          "4" => 42,
-          "5" => 49,
-          "6" => 18,
-          "7" => 27,
-          "8" => 25,
-          "9" => 24
-        }
+      expected_response =   %{
+        "foods" => %{"açaí" => 1, "churrasco" => 2, "esfirra" => 3, "hambúrguer" => 2, "pastel" => 0, "pizza" => 2, "prato_feito" => 0, "sushi" => 0},
+        "users" => %{"1" => 48, "10" => 36, "11" => 0, "12" => 0, "13" => 0, "14" => 0, "15" => 0, "16" => 0, "17" => 0, "18" => 0, "19" => 0, "2" => 45, "20" => 0, "21" => 0, "22" => 0, "23" => 0, "24" => 0, "25" => 0, "26" => 0, "27" => 0, "28" => 0, "29" => 0, "3" => 31, "30" => 0, "4" => 42, "5" => 49, "6" => 18, "7" => 27, "8" => 25, "9" => 24}
       }
 
       # ASSERTION
@@ -98,6 +58,20 @@ defmodule ReportsGeneratorTest do
         |> ReportsGenerator.fetch_higher_cost(option)
 
       expected_response = {:error, "inválid option!"}
+
+      assert response == expected_response
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "list summing using parallelism" do
+      filenames = ["report_1.csv", "report_2.csv", "report_3.csv"]
+
+      response =
+        filenames
+        |> ReportsGenerator.build_from_many()
+
+      expected_response = {:ok, %{"foods" => %{"açaí" => 37742, "churrasco" => 37650, "esfirra" => 37462, "hambúrguer" => 37577, "pastel" => 37392, "pizza" => 37365, "prato_feito" => 37519, "sushi" => 37293}, "users" => %{"1" => 278849, "10" => 268317, "11" => 268877, "12" => 276306, "13" => 282953, "14" => 277084, "15" => 280105, "16" => 271831, "17" => 272883, "18" => 271421, "19" => 277720, "2" => 271031, "20" => 273446, "21" => 275026, "22" => 278025, "23" => 276523, "24" => 274481, "25" => 274512, "26" => 274199, "27" => 278001, "28" => 274256, "29" => 273030, "3" => 272250, "30" => 275978, "4" => 277054, "5" => 270926, "6" => 272053, "7" => 273112, "8" => 275161, "9" => 274003}}}
 
       assert response == expected_response
     end
