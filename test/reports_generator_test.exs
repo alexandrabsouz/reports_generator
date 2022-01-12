15cfd5bbc9558730882a4b85a4406d1712a5d8bd
alexandra <alexandrabsouz@gmail.com>
2022-01-11 21:13:45 -0300

    test: whe a file list is not provided, return a error

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -105,58 +105,72 @@ defmodule ReportsGeneratorTest do
 
   describe "build_from_many/1" do
     test "list summing using parallelism" do
-      filenames = ["report_1.csv", "report_2.csv", "report_3.csv"]
+      filenames = ["report_test.csv", "report_test.csv"]
 
       response =
         filenames
         |> ReportsGenerator.build_from_many()
 
       expected_response =
-        {:ok,
-         %{
-           "foods" => %{
-             "açaí" => 37742,
-             "churrasco" => 37650,
-             "esfirra" => 37462,
-             "hambúrguer" => 37577,
-             "pastel" => 37392,
-             "pizza" => 37365,
-             "prato_feito" => 37519,
-             "sushi" => 37293
-           },
-           "users" => %{
-             "1" => 278_849,
-             "10" => 268_317,
-             "11" => 268_877,
-             "12" => 276_306,
-             "13" => 282_953,
-             "14" => 277_084,
-             "15" => 280_105,
-             "16" => 271_831,
-             "17" => 272_883,
-             "18" => 271_421,
-             "19" => 277_720,
-             "2" => 271_031,
-             "20" => 273_446,
-             "21" => 275_026,
-             "22" => 278_025,
-             "23" => 276_523,
-             "24" => 274_481,
-             "25" => 274_512,
-             "26" => 274_199,
-             "27" => 278_001,
-             "28" => 274_256,
-             "29" => 273_030,
-             "3" => 272_250,
-             "30" => 275_978,
-             "4" => 277_054,
-             "5" => 270_926,
-             "6" => 272_053,
-             "7" => 273_112,
-             "8" => 275_161,
-             "9" => 274_003
-           }
-         }}
+      {
+        :ok,
+        %{
+          "foods" => %{
+            "açaí" => 2,
+            "churrasco" => 4,
+            "esfirra" => 6,
+            "hambúrguer" => 4,
+            "pastel" => 0,
+            "pizza" => 4,
+            "prato_feito" => 0,
+            "sushi" => 0
+          },
+          "users" => %{
+            "1" => 96,
+            "10" => 72,
+            "11" => 0,
+            "12" => 0,
+            "13" => 0,
+            "14" => 0,
+            "15" => 0,
+            "16" => 0,
+            "17" => 0,
+            "18" => 0,
+            "19" => 0,
+            "2" => 90,
+            "20" => 0,
+            "21" => 0,
+            "22" => 0,
+            "23" => 0,
+            "24" => 0,
+            "25" => 0,
+            "26" => 0,
+            "27" => 0,
+            "28" => 0,
+            "29" => 0,
+            "3" => 62,
+            "30" => 0,
+            "4" => 84,
+            "5" => 98,
+            "6" => 36,
+            "7" => 54,
+            "8" => 50,
+            "9" => 48
+          }
+        }
+      }
+
+      assert response == expected_response
+    end
+
+    test "whe a file list is not provided, return a error" do
+      filenames = "banana"
+
+      response =
+        filenames
+        |> ReportsGenerator.build_from_many()
+
+      expected_response = {:error, "not is a filenames list"}
 
       assert response == expected_response
     end
